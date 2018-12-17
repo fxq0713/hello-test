7560d9bad25c747973025d9e36911b970c50daf6
chenghongqiang <962691576@qq.com>
2018-12-17 14:31:27 +0800

    Initial commit

---
+++ src/main/java/com/chq/demo/hellotest/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.chq.demo.hellotest.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试
+ *
+ * @author CHQ
+ */
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return "Hello World";
+    }
+}
